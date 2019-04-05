432e86fc79ff651d57cd95864e347d5c89bcd635
Rob Howell <rob@robhowell.com>
2019-04-05 20:23:35 +0100

    Update to latest dependencies

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const SSH2Promise = require('ssh2-promise');
- 
+
 const ssh = new SSH2Promise({
   host: '192.168.1.134',
   username: 'root',
   password: '1'
 });
- 
+
 const main = async () => {
   await ssh.connect();
-  console.log("Connected to MiSTer");
+  console.log('Connected to MiSTer');
 
   const sftp = ssh.sftp();
-  const directories = await sftp.readdir("/")
-  const dirNames = directories.map(directory => directory.filename)
+  const directories = await sftp.readdir('/');
+  const dirNames = directories.map(directory => directory.filename);
   console.dir(dirNames);
 };
 
